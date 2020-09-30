9d9430cd2a8f54d4a234d49193790ffb8a3fa3ee
pray-753391 <66723274+pray-753391@users.noreply.github.com>
2020-09-30 13:48:04 +0800

    Update 截取完整版.py

--- 其它/截取完整版.py
+++ 其它/截取完整版.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 path = "C:\\Users\\yjr\\Desktop\\temproot.csv"
 index = 0
 data_set = pd.read_csv(path)
@@ -13,11 +14,14 @@ outpath3 = ".csv"
 
 MyArray = MyArray[0:20]
 index=0
+Rightformat = '%Y/%m/%d'
+
 for i in MyArray:
+    NewArray = []
     name = i.head(1)['GOOD_CODE'].to_frame()
     name = name.at[index,'GOOD_CODE']
     name = name.astype(int)
-    i['DTIME'] = pd.to_datetime(i['DTIME']);
+    i.loc[:,'DTIME'] = pd.to_datetime(i.loc[:,'DTIME']);
     i = i.sort_values(by='DTIME',ascending=False)
     outpath = outpath1+str(name)+outpath3
     i.to_csv(outpath,index=False,header=True)
