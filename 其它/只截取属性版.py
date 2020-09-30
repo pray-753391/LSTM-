830be84f272a67c26c4b7fc3075506faee739243
pray-753391 <66723274+pray-753391@users.noreply.github.com>
2020-09-30 13:47:50 +0800

    Update 只截取属性版.py
    会报错 但管他呢 反正能正常运行

--- 其它/只截取属性版.py
+++ 其它/只截取属性版.py
@@ -1,6 +1,6 @@
-
 import numpy as np
 import pandas as pd
+
 path = "C:\\Users\\yjr\\Desktop\\temproot.csv"
 data_set = pd.read_csv(path)
 
@@ -51,9 +51,7 @@ index=0
 for i in MyArray:
     outpath = outpath1+str(Name[index])+outpath3
     index+=1
-    i['DTIME'] = pd.to_datetime(i['DTIME']);
+    i.loc[:,'DTIME'] = pd.to_datetime(i.loc[:,'DTIME']);
     i = i.sort_values(by='DTIME', ascending=False)
     i = i.drop(['DTIME'],axis=1)
     i.to_csv(outpath,index=False,header=True)
-
-
